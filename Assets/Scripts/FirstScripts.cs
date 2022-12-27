9bda4014d1e50268060501154f75d985557673f1
Julee13 <119415263+Julee13@users.noreply.github.com>
2022-12-27 21:20:13 +0800

    Update認識腳本

---
+++ Assets/Scripts/FirstScripts.cs
@@ -0,0 +1,12 @@
+﻿// 單行註解:說明
+// Julee 2022.12.27
+// 可放在程式上下或後面
+
+using UnityEngine;  // 引用命名空間(倉庫) Unity 遊戲引擎(API)
+
+// class 類別 等同於腳本 Script
+// class 後面為腳本名稱須與檔案同名
+public class FirstScripts : MonoBehaviour
+{
+    // 大括號放置腳本內容
+}
