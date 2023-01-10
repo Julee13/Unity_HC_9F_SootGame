ca1bef13e0e4c00ffb26a60354824eeb32c38013
Julee13 <119415263+Julee13@users.noreply.github.com>
2023-01-10 20:02:29 +0800

    Update認識事件與欄位

--- Assets/Scripts/FirstScripts.cs
+++ Assets/Scripts/FirstScripts.cs
@@ -8,5 +8,34 @@ using UnityEngine;  // 引用命名空間(倉庫) Unity 遊戲引擎(API)
 // class 後面為腳本名稱須與檔案同名
 public class FirstScripts : MonoBehaviour
 {
+    // Tab 縮排:大括號enter下一行
+    // 格式化:Ctrl + K D (整理排版快捷鍵)
+
     // 大括號放置腳本內容
+    private void Awake()
+    {
+        //此處要縮排兩次
+
+        //輸出:將訊息顯示在Unity的控制台Console(Ctrl+shift+C)
+        //輸出("文字訊息") 
+        print("哈囉");
+        print("2023");
+        print("<color=#008080ff>新年快樂!</color>");
+    }
+
+    // 開始事件:在喚醒事件後執行一次
+    private void Start()
+    {
+        print("<color=red>開始事件!</color>");
+
+        // 計算1+2
+        print("輸出1+2");
+        print($"輸出{1 + 2}");
+    }
+
+    // 更新事件:一秒執行約60次，約60FPS
+    private void Update()
+    {
+        print("<color=#3333ff>這是更新事件!</color>");
+    }
 }
---
+++ Assets/Scripts/LearnField.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Julee
+{
+    /// <summary>
+    /// 摘要:不是必要但很重要!
+    /// 學習欄位Field
+    /// </summary>
+    public class LearnField : MonoBehaviour
+    {
+
+    }
+}
