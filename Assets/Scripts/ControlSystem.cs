de4382dbae0d978b29e261d536e1229cc0465aa9
Julee13 <119415263+Julee13@users.noreply.github.com>
2023-03-02 21:31:09 +0800

    Update_學習控制系統滑鼠座標_23-0302

--- Assets/Scripts/ControlSystem.cs
+++ Assets/Scripts/ControlSystem.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;    // 條件 1
 
 namespace Julee
 {
@@ -22,6 +23,8 @@ namespace Julee
         private Transform pointSpawn;
         [SerializeField, Header("箭頭")]
         private GameObject objArrow;
+        [SerializeField, Header("滑鼠座標")]
+        private Transform pointMouse;
 
         private string parAttack = "觸發攻擊";
         #endregion
@@ -29,12 +32,12 @@ namespace Julee
         #region 事件
         private void Awake()
         {
-            SpawnMarble();
+            
         }
 
         private void Update()
         {
-            
+            InputManager();
         }
         #endregion
 
@@ -42,7 +45,8 @@ namespace Julee
         /// <summary>
         /// 生成彈珠
         /// </summary>
-        private void SpawnMarble()
+        // 條件 2
+        private IEnumerator SpawnMarble()
         {
             for (int i = 0; i < contMarbleTotal; i++)
             {
@@ -50,8 +54,66 @@ namespace Julee
 
                 // 生成(物件，座標，角度)；
                 // 生成(彈珠預置物，生成位置的座標，生成位置的角度)；
-                Instantiate(prefabMarble, pointSpawn.position, pointSpawn.rotation);
+                GameObject tempMarble = Instantiate(prefabMarble, pointSpawn.position, pointSpawn.rotation);
+
+                // 取得元件<>()
+                // GetComponent<元件名稱>()
+
+                // 暫存彈珠.取得元件<剛體>()
+                tempMarble.GetComponent<Rigidbody>().AddForce(0, 0, speedMarble);
+
+                // 條件 3
+                yield return new WaitForSeconds(intervalshoot);
             }
+        }
+
+        /// <summary>
+        /// 輸入管理:偵測玩家的滑鼠(觸控)
+        /// </summary>
+        private void InputManager()
+        {
+            // 如果 按下 左鍵 顯示箭頭
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                print("<color=#66ff99>玩家按下左鍵</color>");
+
+                objArrow.SetActive(true);                            // 顯示箭頭
+            }
+            else if (Input.GetKey(KeyCode.Mouse0))
+            {
+                print("<color=#6699ff>玩家按住左鍵</color>");
+
+                MouseToWorld();
+            }
+            // 如果 放開 左鍵 隱藏箭頭
+            else if (Input.GetKeyUp(KeyCode.Mouse0))
+            {
+                print("<color=#ff6699>玩家放開左鍵</color>");
+
+                objArrow.SetActive(false);                           // 隱藏箭頭
+
+                // 條件 4
+                StartCoroutine(SpawnMarble());                        // 生成彈珠
+            }
+        }
+
+        /// <summary>
+        /// 2D 滑鼠座標轉為 3D 世界座標
+        /// </summary>
+        private void MouseToWorld()
+        {
+            // print($"<color=#ffff66>滑鼠座標:{ Input.mousePosition } </color>");
+
+            Vector3 posMouse = Input.mousePosition;    // 滑鼠座標
+
+            posMouse.z = 15;   // = MainCamera Y
+
+            // 攝影機.主要的.螢幕轉為世界座標(滑鼠座標)
+            Vector3 posWorld = Camera.main.ScreenToWorldPoint(posMouse);
+
+            // print($"<color=#ffff66>世界座標: { posWorld } </color>");
+
+            pointMouse.position = posWorld;       // 滑鼠座標物件.座標 = 世界座標
         }
         #endregion
     }
