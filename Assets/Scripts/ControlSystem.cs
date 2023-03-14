670c7e317473798c3722ba612e80734db583167d
Julee13 <119415263+Julee13@users.noreply.github.com>
2023-03-14 21:30:37 +0800

    Update生成系統_23-0314

--- Assets/Scripts/ControlSystem.cs
+++ Assets/Scripts/ControlSystem.cs
@@ -59,8 +59,14 @@ namespace Julee
                 // 取得元件<>()
                 // GetComponent<元件名稱>()
 
-                // 暫存彈珠.取得元件<剛體>()
-                tempMarble.GetComponent<Rigidbody>().AddForce(0, 0, speedMarble);
+                // 暫存彈珠.取得元件<剛體>().添加推力(X,Y,Z) - 世界座標
+                // tempMarble.GetComponent<Rigidbody>().AddForce(0, 0, speedMarble);
+
+                // 將推力改為角色區域座標
+                // 右方 transforn.right
+                // 上方 transforn.up
+                // 前方 transforn.forward
+                tempMarble.GetComponent<Rigidbody>().AddForce(transform.forward * speedMarble);
 
                 // 條件 3
                 yield return new WaitForSeconds(intervalshoot);
@@ -75,20 +81,20 @@ namespace Julee
             // 如果 按下 左鍵 顯示箭頭
             if (Input.GetKeyDown(KeyCode.Mouse0))
             {
-                print("<color=#66ff99>玩家按下左鍵</color>");
+                // print("<color=#66ff99>玩家按下左鍵</color>");
 
                 objArrow.SetActive(true);                            // 顯示箭頭
             }
             else if (Input.GetKey(KeyCode.Mouse0))
             {
-                print("<color=#6699ff>玩家按住左鍵</color>");
+                // print("<color=#6699ff>玩家按住左鍵</color>");
 
                 MouseToWorld();
             }
             // 如果 放開 左鍵 隱藏箭頭
             else if (Input.GetKeyUp(KeyCode.Mouse0))
             {
-                print("<color=#ff6699>玩家放開左鍵</color>");
+                // print("<color=#ff6699>玩家放開左鍵</color>");
 
                 objArrow.SetActive(false);                           // 隱藏箭頭
 
@@ -113,7 +119,17 @@ namespace Julee
 
             // print($"<color=#ffff66>世界座標: { posWorld } </color>");
 
-            pointMouse.position = posWorld;       // 滑鼠座標物件.座標 = 世界座標
+            pointMouse.position = posWorld;            // 滑鼠座標物件.座標 = 世界座標
+
+            // 角色面向 滑鼠座標物件
+            // 取得此腳本物件的 Transform 寫法:transform
+            transform.LookAt(posWorld);               
+
+            // 鎖定角色 X 與 Z 軸
+            Vector3 angle = transform.eulerAngles;     // 取得角色座標
+            angle.x = 0;                               // x 歸零
+            angle.z = 0;                               // z 歸零
+            transform.eulerAngles = angle;             // 角色座標 = 新的座標
         }
         #endregion
     }
---
+++ Assets/Scripts/SpawnSystem.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Linq;                 // 陣列 轉 清單
+using System.Collections.Generic;  // 清單
+
+namespace Julee
+{
+    /// <summary>
+    /// 生成系統
+    /// </summary>
+    public class SpawnSystem : MonoBehaviour
+    {
+        #region 資料
+        [SerializeField, Header("怪物陣列")]
+        private GameObject[] prefabEnemys;
+        [SerializeField, Header("第二排的格子")]
+        private Transform[] boxSecond;
+        [Header("生成怪物的最小值與最大值")]
+        [SerializeField, Range(2, 3)]
+        private int countMin = 2;
+        [SerializeField, Range(4, 6)]
+        private int countMax = 5;
+        [SerializeField, Header("要打亂的清單")]
+        private List<Transform> boxRandom = new List<Transform>();   // 清單 List
+        [SerializeField, Header("可以吃的彈珠")]
+        private GameObject prefabMarbleCanEat;
+        #endregion
+
+        #region 事件
+        private void Awake()
+        {
+            Spawn();
+        }
+        #endregion
+
+        #region 方法
+        /// <summary>
+        /// 生成怪物
+        /// </summary>
+        private void Spawn()
+        {
+            int random = Random.Range(countMin, countMax + 1);
+            print($"<color=#ff6699>要生成的怪物數量:{ random }</color>");
+
+            int countToDelete = boxSecond.Length - random;
+            print($"<color=#ff6699>要刪除的格子數量:{ countToDelete }</color>");
+
+            // 1. 打亂清單
+            // 清單 = 陣列.轉為清單
+            boxRandom = boxSecond.ToList();
+            // 新增一個隨機物件，處理隨機行為
+            System.Random randomObject = new System.Random();
+            // 打亂清單
+            // 要打亂的清單 = 要打亂的清單.排序(每一筆資料 => 隨機物件.下一個()).轉回清單
+            boxRandom = boxRandom.OrderBy(x => randomObject.Next()).ToList();
+
+            // 2. 刪除清單不要的格子
+            for (int i = 0; i < countToDelete; i++)
+            {
+                boxRandom.RemoveAt(0);
+            }
+
+            SpawnEnemy();
+        }
+
+        /// <summary>
+        /// 生成敵人預置物
+        /// </summary>
+        private void SpawnEnemy()
+        {
+            for (int i = 0; i < boxRandom.Count; i++)
+            {
+                int random = Random.Range(0, prefabEnemys.Length);
+
+                // 如果 i 等於 0 (第一隻) 就 生成 可以吃的彈珠
+                if (i == 0)
+                {
+                    Instantiate(
+                        prefabMarbleCanEat,
+                        boxRandom[i].position + new Vector3(0, 2, 0),     // 座標
+                        Quaternion.identity);
+                }
+                // 否則
+                else
+                {
+                    Instantiate(
+                        prefabEnemys[random],
+                        boxRandom[i].position + new Vector3(0, 2, 0),     // 座標
+                        Quaternion.identity);
+                }
+            }
+        }
+        #endregion
+    }
+}
