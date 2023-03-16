8d22b9d1d30e6649e1fbf1594ca7aee3638e1014
Julee13 <119415263+Julee13@users.noreply.github.com>
2023-03-16 21:16:27 +0800

    Update傷害系統_23-0316

---
+++ Assets/Scripts/DataBasic.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Julee
+{
+    /// <summary>
+    /// 基本資料:血量與攻擊力
+    /// </summary>
+    //  腳本畫物件 ScriptableObject
+    //  1. 將繼承類別改為 ScriptableObject
+    //  2. 添加類別屬性 CreateAssetMenu
+    //  3. 屬性內使用 menuName 自訂菜單名稱
+
+    [CreateAssetMenu(menuName = "Julee/Data Basic")]
+    public class DataBasic : ScriptableObject
+    {
+        #region 資料
+        [Header("血量"), Range(0, 1000)]
+        public float hp = 200;
+        [Header("攻擊力"), Range(10, 500)]
+        public float attack = 50; 
+        #endregion
+
+
+    }
+}
