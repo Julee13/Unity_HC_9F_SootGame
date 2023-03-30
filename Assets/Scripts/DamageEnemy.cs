4c3ac10eb52e7c7a8a7a837cbc5296585bdf3ece
Julee13 <119415263+Julee13@users.noreply.github.com>
2023-03-30 21:03:05 +0800

    Update_金幣飛行

--- Assets/Scripts/DamageEnemy.cs
+++ Assets/Scripts/DamageEnemy.cs
@@ -12,6 +12,10 @@ namespace Julee
     {
         private string nameMarble = "彈珠";
 
+        // => Lambda 黏巴達
+        // 轉換資料類型
+        // (想要轉換的資料類型)資料名稱
+        private DataEnemy dataEnemy => (DataEnemy)data;
 
         // 1. 怪物與彈珠都要有 Collider
         // 2. 怪物與彈珠其中一個要有 Rigibody
@@ -25,6 +29,39 @@ namespace Julee
             if (collision.gameObject.name.Contains(nameMarble))
             {
                 GetDamage(100);
+            }
+        }
+
+        // override 加上空格後可以選取可覆寫的內容
+        protected override void Dead()
+        {
+            // base 父類別原有的內容
+            base.Dead();
+
+            // 呼叫掉落金幣
+            DropCoin();
+
+            // 刪除此物件
+            Destroy(gameObject);
+        }
+
+        /// <summary>
+        /// 掉落金幣
+        /// </summary>
+        private void DropCoin()
+        {
+            // 判定是否再掉落機率類
+            if (Random.value <= dataEnemy.coinProbability)
+            {
+                for (int i = 0; i < dataEnemy.coinDropCount; i++)
+                {
+                    int angle = Random.Range(0, 360);
+                    // 就生成金幣在怪物頭上
+                    Instantiate(
+                        dataEnemy.prefabCoin,                              // 金幣
+                        transform.position + new Vector3(0, 1.5f, 0),      // 座標 + 位移
+                        Quaternion.Euler(90, angle, 0));                   // 歐拉角度(X，Y，Z)
+                }
             }
         }
     }
